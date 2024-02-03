0dc875a11a0e2f379af3fcaf5ebcd14791100691
mystry22 <alanemehenry6@gmail.com>
2024-02-03 13:16:46 +0100

    another update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { UserModule } from './Modules/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose'; 
 import { AdminModule } from './Modules/admin/admin.module';
-import { MONGODB_DB_CONNECTION } from 'src/common/config/env.config';
+
 
 @Module({
   imports: [UserModule,
